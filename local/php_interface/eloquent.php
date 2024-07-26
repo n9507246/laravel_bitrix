558bb40861dea0175d09ceb9c7dfe963f09a6e5f
root <root@3241811-hp63173.twc1.net>
2024-07-26 16:17:02 +0300

    предварительная настройка orm

---
+++ local/php_interface/eloquent.php
@@ -0,0 +1,13 @@
+<?php
+
+// require $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/init.php';
+
+// use Illuminate\Database\Eloquent\Model;
+
+// class User extends Model {
+//     protected $table = 'users';
+// }
+
+// // Пример использования
+// $user = User::find(1);
+// echo $user->name;
---
+++ local/php_interface/init.php
@@ -0,0 +1,25 @@
+<?php
+
+// use Illuminate\Database\Capsule\Manager as Capsule;
+
+// // Автозагрузка классов через Composer
+// require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+// $capsule = new Capsule;
+
+// $capsule->addConnection([
+//     'driver'    => 'mysql',  // или другой драйвер, например 'sqlite', 'pgsql', 'sqlsrv'
+//     'host'      => 'localhost',
+//     'database'  => 'sitemanager',
+//     'username'  => 'bitrix0',
+//     'password'  => 'Cuip0JVj-Om!gc)?M6w(',
+//     // 'charset'   => 'utf8',
+//     // 'collation' => 'utf8_unicode_ci',
+//     // 'prefix'    => '',
+// ]);
+
+// // Установить глобальный доступ к менеджеру
+// $capsule->setAsGlobal();
+
+// // Настроить Eloquent ORM
+// $capsule->bootEloquent();
